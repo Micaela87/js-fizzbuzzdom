0acee3cf012a2c8d57fcb2d20240db862d5f51e2
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-10-25 14:46:17 +0200

    ex completed

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,22 @@
-const gameContainer = document.querySelector('.container');
-
+const gameContainer = document.querySelector('.square-container');
 
 for (let i = 1; i <= 100; i++) {
+    let square = document.createElement('div');
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        console.log(i, 'divisibile per 5 e per 3')
+        square.classList.add('fizzbuzz');
+        square.innerHTML = 'fizzbuzz';
+        gameContainer.append(square);
     } else if (i % 3 == 0) {
-        console.log(i, 'divisibile solo per 3')
+        square.classList.add('fizz');
+        square.innerHTML = 'fizz';
+        gameContainer.append(square);
     } else if (i % 5 == 0) {
-        console.log(i, 'divisibile solo per 5')
+        square.classList.add('buzz');
+        square.innerHTML = 'buzz';
+        gameContainer.append(square);
+    } else {
+        square.classList.add('normal');
+        square.innerHTML = i;
+        gameContainer.append(square);
     }
-}
+};
