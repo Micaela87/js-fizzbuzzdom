5d7ae38fc2fcec65c99f6cecc231fb5d3f6d02a9
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-10-25 14:51:04 +0200

    ex revised

--- public/js/main.js
+++ public/js/main.js
@@ -2,21 +2,19 @@ const gameContainer = document.querySelector('.square-container');
 
 for (let i = 1; i <= 100; i++) {
     let square = document.createElement('div');
+    square.classList.add('normal');
+    square.innerHTML = i;
     if ((i % 3 == 0) && (i % 5 == 0)) {
         square.classList.add('fizzbuzz');
         square.innerHTML = 'fizzbuzz';
-        gameContainer.append(square);
     } else if (i % 3 == 0) {
         square.classList.add('fizz');
         square.innerHTML = 'fizz';
-        gameContainer.append(square);
     } else if (i % 5 == 0) {
         square.classList.add('buzz');
         square.innerHTML = 'buzz';
         gameContainer.append(square);
-    } else {
-        square.classList.add('normal');
-        square.innerHTML = i;
-        gameContainer.append(square);
     }
+
+    gameContainer.append(square);
 };
